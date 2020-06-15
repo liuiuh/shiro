1f9109c646eb04b1cadf27d0efffe1b7db783733
liu <jianhui.liu@luckincoffee.com>
2020-06-15 10:36:53 +0800

    常量

---
+++ src/main/java/com/example/shiro/config/Constant.java
@@ -0,0 +1,10 @@
+package com.example.shiro.config;
+
+/**
+ * @Description:  常量
+ * @Author:  Liu
+ * @Date: 2020/6/15 09:31
+ */
+public class Constant {
+    public static final String POST ="POST" ;
+}
